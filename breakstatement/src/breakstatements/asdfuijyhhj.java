4c1502d9a7c1a6b5b7c9f85b4bdf62a2f3b9fef1
User <richard.nguyen.ak@gmail.com>
2024-08-20 12:33:53 -0500

    guh

---
+++ breakstatement/src/breakstatements/asdfuijyhhj.java
@@ -0,0 +1,20 @@
+package breakstatements;
+
+// int a=1; = uses when we assign variable
+// == uses to compare the exact value
+public class asdfuijyhhj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int x=1; x<=10; x++) {
+			if(x==7) {
+				break;
+			}
+			System.out.println(x);
+		}
+	}
+
+}
+// break terminates the lopp to go further
+
+
