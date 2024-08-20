a6896d08ca893f47377250392fb5d8f4ff0be18c
User <richard.nguyen.ak@gmail.com>
2024-08-20 12:26:22 -0500

    guh

---
+++ array/src/arraies/aszdfghjhuytewr.java
@@ -0,0 +1,16 @@
+package arraies;
+import java.util.Arrays;
+
+public class aszdfghjhuytewr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr= {5, 6,8,11 };
+		System.out.println(Arrays.toString(arr));
+		String [] colors= {"red","blue","purple","black"};
+		colors[3]="white";
+		System.out.println(Arrays.toString(colors));
+		System.out.println(colors.length);
+	}
+
+}
