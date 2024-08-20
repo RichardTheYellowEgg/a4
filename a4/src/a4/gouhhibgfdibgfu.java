37c51d2022b1bc23bc4569876d63bf1e4116275d
User <richard.nguyen.ak@gmail.com>
2024-08-20 12:12:47 -0500

    guh

---
+++ a4/src/a4/gouhhibgfdibgfu.java
@@ -0,0 +1,34 @@
+package a4;
+import java.util.Arrays;
+import java.util.Scanner;
+public class gouhhibgfdibgfu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner guh = new Scanner(System.in);
+		System.out.println("gimmie number");
+		int x= guh.nextInt();
+		System.out.println("gimmie number");
+		int y= guh.nextInt();
+		System.out.println("gimmie number");
+		int z= guh.nextInt();
+		System.out.println("gimmie number");
+		int w= guh.nextInt();
+		System.out.println("gimmie number");
+		int v= guh.nextInt();
+		int o [] = {x,y,z,w,v};
+		int p= 0;
+		for(int r=0;r<o.length; r++){
+			p=p+ o[0];
+		}
+		System.out.println(p);
+		//System.out.println(Arrays.toString(o));
+		double sum = 0;
+		for(int i = 0; i<o.length; i++) {
+			sum=sum+ o[0];
+		}
+		double u=sum/o.length;
+		System.out.println(z);
+	}
+
+}
