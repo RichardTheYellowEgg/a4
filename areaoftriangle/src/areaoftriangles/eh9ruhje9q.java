229c6e644a12af77653d95a49d3a7245536979fa
User <richard.nguyen.ak@gmail.com>
2024-08-20 12:24:30 -0500

    guh

---
+++ areaoftriangle/src/areaoftriangles/eh9ruhje9q.java
@@ -0,0 +1,19 @@
+package areaoftriangles;
+import java.util.Scanner;
+public class eh9ruhje9q {
+	static double guh(double o, double g) {
+		return (o*g)/2;
+		//System.out.println(x);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner e = new Scanner(System.in);
+		System.out.println("gimmie a hight ");
+		double o = e.nextInt();
+		System.out.println("gimmie width ");
+		double g = e.nextInt();
+		System.out.println("the area is "+guh(o, g));
+	}
+
+}
