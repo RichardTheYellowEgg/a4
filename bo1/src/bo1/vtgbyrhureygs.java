5ac96a8e69d726e7346bc666244a685f2c839351
User <richard.nguyen.ak@gmail.com>
2024-08-20 12:33:25 -0500

    guh

---
+++ bo1/src/bo1/vtgbyrhureygs.java
@@ -0,0 +1,19 @@
+package bo1;
+import java.util.Scanner;
+public class vtgbyrhureygs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner yuh = new Scanner(System.in);
+		System.out.println("gimmie number");
+		int x= yuh.nextInt();
+		System.out.println("gimmie number");
+		int y= yuh.nextInt();
+		int a = x+y;
+		int b = x-y;
+		int c = x*y;
+		int z = x/y;
+		System.out.println(a+" "+b+" "+c+" "+z);
+	}
+
+}
