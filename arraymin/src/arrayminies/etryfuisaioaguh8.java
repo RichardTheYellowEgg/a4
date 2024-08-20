a6e27a55da9f8ee66a422ce496d67ce17992890d
User <richard.nguyen.ak@gmail.com>
2024-08-20 12:29:56 -0500

    guh

---
+++ arraymin/src/arrayminies/etryfuisaioaguh8.java
@@ -0,0 +1,11 @@
+package arrayminies;
+import java.util.Arrays;
+public class etryfuisaioaguh8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] x = {1,2,3,5,7,11,13,17};
+		int min = x[0];
+	}
+
+}
