0b9083143522e9bb35d9307ae05d8f663c347bf4
User <richard.nguyen.ak@gmail.com>
2024-08-20 12:20:44 -0500

    guh

---
+++ AdditionGoBeyond/src/qwetgdfg/etgbfhnjmjney5k.java
@@ -0,0 +1,17 @@
+package qwetgdfg;
+import java.util.Scanner;
+public class etgbfhnjmjney5k {
+
+	public static void main(String[] args) {
+
+		int sum=0;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("put a positive whole number: ");
+		int a = sc.nextInt();
+		for(int x=1; x<=a; x++) {
+			sum+=x;
+		}
+		System.out.println(sum);
+	}
+
+}
