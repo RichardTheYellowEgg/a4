645eb1d14813e7a96fd1dc5dd7615ab29e9d39eb
User <richard.nguyen.ak@gmail.com>
2024-08-20 12:27:24 -0500

    guh

---
+++ arrayaverage/src/arrayaverage/oiuhtrionhj.java
@@ -0,0 +1,17 @@
+package arrayaverage;
+import java.util.Arrays;
+public class oiuhtrionhj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] x = new int [] {5,4,3,4,5};
+		System.out.println(Arrays.toString(x));
+		double sum = 0;
+		for(int i = 0; i<x.length; i++) {
+			sum=sum+ x[0];
+		}
+		double z=sum/x.length;
+		System.out.println(z);
+	}
+
+}
