95121aa148ac0298711452720ef1b9075f4a9e94
User <richard.nguyen.ak@gmail.com>
2024-08-21 12:33:36 -0500

    guh

---
+++ forloop/src/forloopy/sxzdcrfgvhw.java
@@ -0,0 +1,22 @@
+package forloopy;
+
+public class sxzdcrfgvhw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*for loop:
+			1. initialization
+			2. condition
+			3. increment/decrement
+			4. statement*/
+		int start = 1;
+		int end = 10;
+		for(int x = start; x <= end; x++) {
+			if(x%2 !=0) {
+				System.out.println(x);
+			}
+		}
+	}
+
+}
