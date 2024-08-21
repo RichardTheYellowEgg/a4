150b2b11a545a6b85753d2fa13666ca0a02b5ff4
User <richard.nguyen.ak@gmail.com>
2024-08-21 12:31:54 -0500

    guh

---
+++ cs2_NotTheGame/src/cs2_NotTheGame/ghfupsdfhguuhiogf.java
@@ -0,0 +1,21 @@
+package cs2_NotTheGame;
+import java.util.Scanner;
+public class ghfupsdfhguuhiogf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner guh=new Scanner(System.in);
+		System.out.println("gimmie number");
+		int x= guh.nextInt();
+		if(x<0) {
+			System.out.println("negative");
+		}
+		else if(x>0) {
+			System.out.println("positive");
+		}
+		else {
+			System.out.println("0");
+		}
+	}
+
+}
