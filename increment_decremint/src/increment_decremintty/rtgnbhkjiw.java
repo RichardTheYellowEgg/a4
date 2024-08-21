7637e92ec7362798bd28234c284583465ec00e9e
User <richard.nguyen.ak@gmail.com>
2024-08-21 12:35:05 -0500

    guh

---
+++ increment_decremint/src/increment_decremintty/rtgnbhkjiw.java
@@ -0,0 +1,22 @@
+package increment_decremintty;
+
+public class rtgnbhkjiw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*+ addition
+		- subtraction
+		* multiplication
+		/ division
+		
+		++ increament the value of x will increace by 1
+		preincreament ++x first the value will add then print
+		postincreament x++ the value will print as it is then 1 will add
+		-- decreament*/
+		
+		int x=5;
+		System.out.println(++x);
+		
+	}
+
+}
