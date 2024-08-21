d5be1f6ec93a5852f93c47b6f31bd4ddaaa7b31f
User <richard.nguyen.ak@gmail.com>
2024-08-21 12:33:00 -0500

    guh

---
+++ firstProgram/src/firstProgram/helloWorld.java
@@ -0,0 +1,25 @@
+package firstProgram;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// variable
+		// by assigning data type
+		//numbers= int/integer
+		//text/characters= string
+		//decimal numbers= float/ double
+		//bool/ booolean= true/false, yes/no
+		// float smaller values
+		// double larger values
+		int b = 5;
+		String a ="hello world";
+		float c = 3.14f;
+		double d= 7.62;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
