5077ca9a00dab7f96619a4b6a153306ce536e8ac
User <richard.nguyen.ak@gmail.com>
2024-08-21 12:31:20 -0500

    guh

---
+++ continues/src/continuer/qedfvfcxvbb.java
@@ -0,0 +1,17 @@
+package continuer;
+
+public class qedfvfcxvbb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int x=1; x<=12; x++) {
+			if(x==11) {
+				continue;
+			}
+			System.out.println(x);
+		}
+
+	}
+
+}
+//continue statement skips
